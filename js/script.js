645cce0a1ba805538d322ef77fbe325ba77bf1f6
Marcelo Gabriel Marochi <marce.marochi@gmail.com>
2025-01-11 23:17:47 -0300

    modifica cant px que se mueve el carrusel segun la resolucion

--- js/script.js
+++ js/script.js
@@ -10,23 +10,28 @@ function App() {}
     const carruselList = event.currentTarget.parentNode;
     const track = event.currentTarget.parentNode.querySelector('#track');
     const carrusel = track.querySelectorAll('.carrusel');
-
-    const carruselWidth = carrusel[0].offsetWidth / 3;
-
+  
+    let carruselWidth;
+    if (window.innerWidth < 768) {
+      carruselWidth = carrusel[0].offsetWidth / 3; // Mover la mitad de la anchura del carrusel para pantallas menores a 768px
+    } else {
+      carruselWidth = carrusel[0].offsetWidth; // Mover más cantidad de píxeles para pantallas mayores a 768px
+    }
+  
     const trackWidth = track.offsetWidth;
     const listWidth = carruselList.offsetWidth;
-
+  
     track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
-
+  
     btn.dataset.button == "button-prev" ? prevAction(leftPosition, carruselWidth, track) : nextAction(leftPosition, trackWidth, listWidth, carruselWidth, track);
   }
-
+  
   let prevAction = (leftPosition, carruselWidth, track) => {
     if (leftPosition > 0) {
       track.style.left = `${-1 * (leftPosition - carruselWidth)}px`;
     }
   }
-
+  
   let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => {
     if (leftPosition < (trackWidth - listWidth)) {
       track.style.left = `${-1 * (leftPosition + carruselWidth)}px`;
